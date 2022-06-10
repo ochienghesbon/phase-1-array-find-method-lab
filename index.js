69914fac3afb97a5892c5c5436452b1887819438
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-11 01:33:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(record){
+      const result=record.find((record)=>record.result==="W")
+      if (result){
+          return result.year;
+
+      }
+  }
+  
+// function superbowlWin(Array){
+//     const record = [
+//         { year: "2015", result: "W"},
+//         { year: "2014", result: "N/A"},
+//         { year: "2013", result: "L"},
+//         //...
+//       ]
+//       for(const record of Array){
+//           if(record.result==="win"){
+//               console.log(record.year);
+//           }
+//       }
+
+// }
+
+    
+
+
+    
+
+
